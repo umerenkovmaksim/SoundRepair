ac7b0a1fe1dce9846edd5bec7484512b1b66b9d9
umerenkovmaksim <umerenkovmaksim7@gmail.com>
2023-03-30 14:09:39 +0300

    Start project

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+HOST = '0.0.0.0'
+PORT = 5000
+WEBSITE_URL = 'http://127.0.0.1:5000/'
+
+
+@app.route('/')
+def main_page():
+    return render_template('main_page.html', title='SoundRepair')
+
+
+app.run(host=HOST, port=PORT)
