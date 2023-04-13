3e78f3f28f9efe21f26b3401240672a290f8d5ff
umerenkovmaksim <umerenkovmaksim7@gmail.com>
2023-04-13 16:18:54 +0300

    Create template

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ WEBSITE_URL = 'http://127.0.0.1:5000/'
 
 @app.route('/')
 def main_page():
-    return render_template('main_page.html', title='SoundRepair')
+    return render_template('index.html', title='SoundRepair')
 
 
 app.run(host=HOST, port=PORT)
