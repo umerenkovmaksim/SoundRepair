0de6300407421c29250dcc7466af64c324b76094
umerenkovmaksim <umerenkovmaksim7@gmail.com>
2023-04-19 16:12:31 +0300

    Was added base template

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 HOST = '0.0.0.0'
 PORT = 5000
-WEBSITE_URL = 'http://127.0.0.1:5000/'
+WEBSITE_URL = 'http://127.0.0.1:5000'
 
 
 @app.route('/')
